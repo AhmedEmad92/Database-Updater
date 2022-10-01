43aa565e3307b44f35e30f8b1dbf972a9b5e3398
Dev-Ahmed-Emad <dr.eng.ahmed@gmail.com>
2022-10-02 00:54:40 +0200

    fix exception when connection closed

--- updater_server.py
+++ updater_server.py
@@ -1,3 +1,4 @@
+from audioop import add
 import zlib
 import socket    as s         
 import threading
@@ -27,182 +28,188 @@ def handle_client(s_conn,addr):
     connected = True
     Er = True
     while connected:
-        msg = pickle.loads(zlib.decompress(s_conn.recv(4096)))
-        # [sqlst,SHOST,SPORT,SUSER,SPASSWORD,SDRIVER,PKEY,S,SDATABASE]
-        if msg:
-            sqlst= msg[0]
-            shost= "localhost"
-            sport=msg[2]
-            suser=msg[3]
-            spass=msg[4]
-            sdrive=msg[5]
-            pkey=msg[6]
-            spkey=msg[6].split (",")
-            n_pkey = len(spkey)
-            s=msg[7]
-            sdatabase=msg[8]
-            limit =  msg[9]
-            done = msg[10]
-            sync=msg[11]
-            col=msg[12]
-            # ,HAS_OBST,OBS
-            has_obs = msg[13]
-            obs=msg[14]
-            sobs=msg[14].split (",")
-            scol = msg[15]
-            arabic = msg[16]
-            debug_= msg[17]
-            nobs=len(sobs)
-            if Er == True :
-                if s=="SQL":
-                    comm = pyodbc.connect('DRIVER='+sdrive+';SERVER='+shost+';DATABASE='+sdatabase+';UID='+suser+';PWD='+spass+';Trusted_Connection=no'+';' )
-                    cursor = comm.cursor()
-                elif s=="ORACLE":
-                    s_tns = cx_Oracle.makedsn(shost, sport, service_name=sdatabase)
-                    comm = cx_Oracle.connect(user=suser, password=spass, dsn=s_tns) 
-                    cursor = comm.cursor()
-                Er = False
-            if done == "1":
-                # cursor = comm.cursor()
-                if s=="SQL":
-                    cursor.fast_executemany = True
-                cursor.execute(sqlst)
-                for ora in cursor:
-                    cnt  = ora[0]
-                print("Remain " + str(cnt))
-                message=pickle.dumps(cnt)
-                message=zlib.compress(message)
-                s_conn.send(message)
-            elif done == " ":
-                cursor.execute(sqlst)
-                rows=cursor.fetchmany(limit)
-                if debug_==1 :
-                    print(str(rows))
-                message=pickle.dumps(rows)
-                message = zlib.compress(message)
-                print("Sending Data to Client ...")
-                s_conn.send(message)
-            elif done =="done":
-                if limit > cnt :
-                    limit = cnt
-                lim=0
-                while lim< limit:
-                    sqlst = "SELECT COUNT(*) FROM  " + sync + " WHERE "
-                    c=0
-                    val=rows[lim]
-                    index_of_pk=0
-                    while n_pkey != c :
-                        index_of_pk=col.index(spkey[c])
-                        if s== "SQL" and arabic == "Y":
-                                sqlst = sqlst +sync+"."+ spkey[c] +"=" + "N'" +str(val[index_of_pk]) + "'"
-                        else:
-                            sqlst = sqlst +sync+"."+ spkey[c] +"=" + "'" +str(val[index_of_pk]) + "'"
-                        c=c+1
-                        if c!=n_pkey:
-                            sqlst=sqlst+ " and "
-                    if debug_==1 :
-                        print(sqlst)
-                    # cursor = comm.cursor()
-                    if s=="SQL":
-                        cursor.fast_executemany = True
-                    cursor.execute(sqlst)
-                    for ora in cursor:
-                            count_ = ora[0]
-                    if count_ == 0 :
-                        sqlst  = "INSERT INTO "+sync +"( "+pkey 
-                        if has_obs == "Y":
-                            sqlst = sqlst +","+obs+" ) VALUES ("
-                        elif has_obs == "N":
-                            sqlst = sqlst + " ) VALUES ("
-                        c=0
-                        index_of_pk=0
-                        while n_pkey != c :
-                            index_of_pk=col.index(spkey[c])
-                            if s == "ORACLE" :
-                                sqlst = sqlst + "'" +str(val[index_of_pk]) +"'"
-                            elif s=="SQL" and arabic == "Y":
-                                sqlst = sqlst + "N'" +str(val[index_of_pk]) +"'"
-                            elif s=="SQL" and arabic == "N":
-                                sqlst = sqlst + "'" +str(val[index_of_pk]) +"'"
-                            c=c+1
-                            if c != n_pkey:
-                                sqlst = sqlst+","
-                            elif has_obs == "N":
-                                sqlst=sqlst + ")"
-                            elif has_obs =="Y":
-                                sqlst=sqlst+","
-                                index_of_obs=0
-                                cc=0
-                                while nobs != cc :
-                                    index_of_obs=col.index(sobs[cc])
-                                    if val[cc]==None and s =="SQL" :
+        comp= s_conn.recv(4096)
+        if comp:
+            decomp= zlib.decompress(comp)
+            if decomp:
+                msg = pickle.loads(decomp)
+                if msg:
+                    sqlst= msg[0]
+                    shost= "localhost"
+                    sport=msg[2]
+                    suser=msg[3]
+                    spass=msg[4]
+                    sdrive=msg[5]
+                    pkey=msg[6]
+                    spkey=msg[6].split (",")
+                    n_pkey = len(spkey)
+                    s=msg[7]
+                    sdatabase=msg[8]
+                    limit =  msg[9]
+                    done = msg[10]
+                    sync=msg[11]
+                    col=msg[12]
+                    # ,HAS_OBST,OBS
+                    has_obs = msg[13]
+                    obs=msg[14]
+                    sobs=msg[14].split (",")
+                    scol = msg[15]
+                    arabic = msg[16]
+                    debug_= msg[17]
+                    nobs=len(sobs)
+                    if Er == True :
+                        if s=="SQL":
+                            comm = pyodbc.connect('DRIVER='+sdrive+';SERVER='+shost+';DATABASE='+sdatabase+';UID='+suser+';PWD='+spass+';Trusted_Connection=no'+';' )
+                            cursor = comm.cursor()
+                        elif s=="ORACLE":
+                            s_tns = cx_Oracle.makedsn(shost, sport, service_name=sdatabase)
+                            comm = cx_Oracle.connect(user=suser, password=spass, dsn=s_tns) 
+                            cursor = comm.cursor()
+                        Er = False
+                    if done == "1":
+                        # cursor = comm.cursor()
+                        if s=="SQL":
+                            cursor.fast_executemany = True
+                        cursor.execute(sqlst)
+                        for ora in cursor:
+                            cnt  = ora[0]
+                        print("Remain " + str(cnt))
+                        message=pickle.dumps(cnt)
+                        message=zlib.compress(message)
+                        s_conn.send(message)
+                    elif done == " ":
+                        cursor.execute(sqlst)
+                        rows=cursor.fetchmany(limit)
+                        if debug_==1 :
+                            print(str(rows))
+                        message=pickle.dumps(rows)
+                        message = zlib.compress(message)
+                        print("Sending Data to Client ...")
+                        s_conn.send(message)
+                    elif done =="done":
+                        if limit > cnt :
+                            limit = cnt
+                        lim=0
+                        while lim< limit:
+                            sqlst = "SELECT COUNT(*) FROM  " + sync + " WHERE "
+                            c=0
+                            val=rows[lim]
+                            index_of_pk=0
+                            while n_pkey != c :
+                                index_of_pk=col.index(spkey[c])
+                                if s== "SQL" and arabic == "Y":
+                                        sqlst = sqlst +sync+"."+ spkey[c] +"=" + "N'" +str(val[index_of_pk]) + "'"
+                                else:
+                                    sqlst = sqlst +sync+"."+ spkey[c] +"=" + "'" +str(val[index_of_pk]) + "'"
+                                c=c+1
+                                if c!=n_pkey:
+                                    sqlst=sqlst+ " and "
+                            if debug_==1 :
+                                print(sqlst)
+                            # cursor = comm.cursor()
+                            if s=="SQL":
+                                cursor.fast_executemany = True
+                            cursor.execute(sqlst)
+                            for ora in cursor:
+                                    count_ = ora[0]
+                            if count_ == 0 :
+                                sqlst  = "INSERT INTO "+sync +"( "+pkey 
+                                if has_obs == "Y":
+                                    sqlst = sqlst +","+obs+" ) VALUES ("
+                                elif has_obs == "N":
+                                    sqlst = sqlst + " ) VALUES ("
+                                c=0
+                                index_of_pk=0
+                                while n_pkey != c :
+                                    index_of_pk=col.index(spkey[c])
+                                    if s == "ORACLE" :
+                                        sqlst = sqlst + "'" +str(val[index_of_pk]) +"'"
+                                    elif s=="SQL" and arabic == "Y":
+                                        sqlst = sqlst + "N'" +str(val[index_of_pk]) +"'"
+                                    elif s=="SQL" and arabic == "N":
+                                        sqlst = sqlst + "'" +str(val[index_of_pk]) +"'"
+                                    c=c+1
+                                    if c != n_pkey:
+                                        sqlst = sqlst+","
+                                    elif has_obs == "N":
+                                        sqlst=sqlst + ")"
+                                    elif has_obs =="Y":
+                                        sqlst=sqlst+","
+                                        index_of_obs=0
+                                        cc=0
+                                        while nobs != cc :
+                                            index_of_obs=col.index(sobs[cc])
+                                            if val[cc]==None and s =="SQL" :
+                                                sqlst = sqlst + "NULL"
+                                                cc=cc+1
+                                            elif val[cc]==None and s =="ORACLE" :
+                                                sqlst = sqlst + "''"
+                                                cc=cc+1
+                                            elif s == "SQL":
+                                                sqlst = sqlst + "N'" +str(val[index_of_obs])  + "'"
+                                                cc=cc+1
+                                            else:
+                                                sqlst = sqlst + "'" +str(val[index_of_obs])  + "'"
+                                                cc=cc+1
+                                            if cc!= nobs:
+                                                sqlst=sqlst+","
+                                            else:
+                                                sqlst=sqlst+")"
+                                if debug_==1 :
+                                    print(sqlst)
+                                # cursor.execute(sqlst)  
+                                # comm.commit()
+                            else:
+                                sqlst = "UPDATE "+sync +" SET "
+                                i=nobs
+                                q=0
+                                while q != i :
+                                    index_of_obs=col.index(sobs[q])
+                                    sqlst = sqlst + sobs[q] +"= " 
+                                    if val[q]==None and s =="SQL" :
                                         sqlst = sqlst + "NULL"
-                                        cc=cc+1
-                                    elif val[cc]==None and s =="ORACLE" :
+                                        q=q+1
+                                    elif val[q]==None and s =="ORACLE" :
                                         sqlst = sqlst + "''"
-                                        cc=cc+1
+                                        q=q+1
                                     elif s == "SQL":
                                         sqlst = sqlst + "N'" +str(val[index_of_obs])  + "'"
-                                        cc=cc+1
+                                        q=q+1
                                     else:
                                         sqlst = sqlst + "'" +str(val[index_of_obs])  + "'"
-                                        cc=cc+1
-                                    if cc!= nobs:
+                                        q=q+1
+                                    if q!= i:
                                         sqlst=sqlst+","
+                                sqlst=sqlst+" WHERE " 
+                                nn=0
+                                index_of_pk=0
+                                while n_pkey != nn :
+                                    index_of_pk=col.index(spkey[nn])
+                                    if s== "SQL" and arabic == "Y":
+                                            sqlst = sqlst +sync+"."+ spkey[nn] +"=" + "N'" +str(val[index_of_pk]) + "'"
                                     else:
-                                        sqlst=sqlst+")"
-                        if debug_==1 :
-                            print(sqlst)
-                        # cursor.execute(sqlst)  
-                        # comm.commit()
-                    else:
-                        sqlst = "UPDATE "+sync +" SET "
-                        i=nobs
-                        q=0
-                        while q != i :
-                            index_of_obs=col.index(sobs[q])
-                            sqlst = sqlst + sobs[q] +"= " 
-                            if val[q]==None and s =="SQL" :
-                                sqlst = sqlst + "NULL"
-                                q=q+1
-                            elif val[q]==None and s =="ORACLE" :
-                                sqlst = sqlst + "''"
-                                q=q+1
-                            elif s == "SQL":
-                                sqlst = sqlst + "N'" +str(val[index_of_obs])  + "'"
-                                q=q+1
-                            else:
-                                sqlst = sqlst + "'" +str(val[index_of_obs])  + "'"
-                                q=q+1
-                            if q!= i:
-                                sqlst=sqlst+","
-                        sqlst=sqlst+" WHERE " 
-                        nn=0
-                        index_of_pk=0
-                        while n_pkey != nn :
-                            index_of_pk=col.index(spkey[nn])
-                            if s== "SQL" and arabic == "Y":
-                                    sqlst = sqlst +sync+"."+ spkey[nn] +"=" + "N'" +str(val[index_of_pk]) + "'"
-                            else:
-                                sqlst = sqlst +sync+"."+ spkey[nn] +"=" + "'" +str(val[index_of_pk]) + "'"
-                            nn=nn+1
-                            if nn!=n_pkey:
-                                sqlst=sqlst+ " and "
-                        if debug_==1 :            
-                            print(sqlst)
-                    if s=="SQL":
-                        cursor.fast_executemany = True
-                    cursor.execute(sqlst)          
-                    lim=lim+1
-                comm.commit()
-                print("commited " + str(limit)+ " rows")
-                message=pickle.dumps([0])
-                message = zlib.compress(message)
-                s_conn.send(message)
+                                        sqlst = sqlst +sync+"."+ spkey[nn] +"=" + "'" +str(val[index_of_pk]) + "'"
+                                    nn=nn+1
+                                    if nn!=n_pkey:
+                                        sqlst=sqlst+ " and "
+                                if debug_==1 :            
+                                    print(sqlst)
+                            if s=="SQL":
+                                cursor.fast_executemany = True
+                            cursor.execute(sqlst)          
+                            lim=lim+1
+                        comm.commit()
+                        print("commited " + str(limit)+ " rows")
+                        message=pickle.dumps([0])
+                        message = zlib.compress(message)
+                        s_conn.send(message)
+        else:
+            connected = False
+            print(f"Disconnected {addr} !")
 
 def start():
     server.listen()
-    print("Updater Server v3.13")
+    print("Updater Server v3.14")
     print(f"LISTENING SERVER is listening on PORT {SERVER} : {PORT}")
     while True:
         s_conn,addr = server.accept()
